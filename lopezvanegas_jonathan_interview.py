617bd49503a9b13d15836cc0712b41d73b71714c
jonathan123170 <jonathan123170@mcpsva.org>
2026-08-28 14:31:21 -0400

    lol

--- lopezvanegas_jonathan_interview.py
+++ lopezvanegas_jonathan_interview.py
@@ -4,3 +4,25 @@ Author: <Lopez Vanegas, Jonathan>
 Created: <08/26/2026>
 Instructor: Mr. Burgess
 """
+
+print("Welcome! Please answer the following questions:")
+
+N = input("How will you handle a stressful situation at work?")
+
+print("Okay, next question!")
+
+A = input("What specific role would you like to apply for?")
+
+print("Nice!")
+
+M = input("How do you see your role supporting the overall success and well-being of the students?")
+
+print("Okay, next question!")
+
+E = input("Our school is diverse, how would you ensure that your daily work and interactions are inclusive and welcoming to everyone")
+
+print("Okay, final question!")
+
+F = input("What questions would like us to answer for you?")
+
+print("Thank you for your patience! We will respond back as soon as possible! Have a great day!")
