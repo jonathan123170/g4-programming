ff489a0902fbeed2cc01d76c584087822cf13537
jonathan123170 <jonathan123170@mcpsva.org>
2026-08-28 15:17:35 -0400

    lol

---
+++ madlibs.py
@@ -0,0 +1,25 @@
+print("\nWelcome to this Madlibs game! \nPlease answer the following questions:")
+
+a = input("\n1.\nType in an adjective:")
+
+b = input("\n2.\nType in a noun that is an animal:")
+
+c = input("\n3.\nType in a past-tense verb:")
+
+d = input("\n4.\nType in an adverb that ends in '-ly':")
+
+e = input("\n5.\nType in a noun that is a place:")
+
+f = input("\n6.\nType in an adjective:")
+
+g = input("\n7.\nType in a plural noun:")
+
+h = input("\n8.\nType in a noun:")
+
+i = input("\n9.\nType in an exclamation/silly word:")
+
+j = input("\n10.\nType in a verb ending in '-ing':")
+
+k = input("\n11.\nType in a liquid:")
+
+l = input("\n12.\nType in a plural body part:")
