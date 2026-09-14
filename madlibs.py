eac54263199e7f5122303adc44a0396ae60b0f43
jonathan123170 <jonathan123170@mcpsva.org>
2026-09-14 11:31:38 -0400

    lol

--- madlibs.py
+++ madlibs.py
@@ -2,24 +2,40 @@ print("\nWelcome to this Madlibs game! \nPlease answer the following questions:"
 
 a = input("\n1.\nType in an adjective:")
 
-b = input("\n2.\nType in a noun that is an animal:")
+b = input("\n2.\nType in a noun:")
 
-c = input("\n3.\nType in a past-tense verb:")
+c = input("\n3.\nType in a verb:")
 
-d = input("\n4.\nType in an adverb that ends in '-ly':")
+d = input("\n4.\nType in a color:")
 
-e = input("\n5.\nType in a noun that is a place:")
+e = input("\n5.\nType in a plural noun:")
 
-f = input("\n6.\nType in an adjective:")
+f = input("\n6.\nType in a noun:")
 
-g = input("\n7.\nType in a plural noun:")
+g = input("\n7.\nType in an adjective:")
 
-h = input("\n8.\nType in a noun:")
+h = input("\n8.\nType in a plural noun:")
 
-i = input("\n9.\nType in an exclamation/silly word:")
+i = input("\n9.\nType in a noun:")
 
-j = input("\n10.\nType in a verb ending in '-ing':")
+j = input("\n10.\nType in a sound effect (e.g, BOOM, POW, WHACK):")
 
-k = input("\n11.\nType in a liquid:")
+k = input("\n11.\nType in a noun")
 
-l = input("\n12.\nType in a plural body part:")
+l = input("\n12.\nType in a verb ending in 'ing':")
+
+m = input("\n13.\nType in a verb:")
+
+n = input("\n14.\nType in a animal:")
+
+o = input("\n15.\nType in a clothing item:")
+
+p = input("\n16.\nType in an exclamation:")
+
+q = input("\n17.\nType in a number:")
+
+r = input("\n18.\nType in a plural noun:")
+
+s = input("\n19.\nType in a noun:")
+
+t = input("\n20.\nType in a place:")
