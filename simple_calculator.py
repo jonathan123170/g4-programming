516722ea331f150626a68fa4c2d7496244c0ca0a
jonathan123170 <jonathan123170@mcpsva.org>
2026-09-14 15:03:35 -0400

    lol

---
+++ simple_calculator.py
@@ -0,0 +1,17 @@
+print("Hello! Welcome to the Simple Calculator!")
+
+print("\nThis simple calculator is a calculator that will help with 2 numbers on the 4 basic symbols:")
+
+print("1. Addition")
+
+print("2. Subtraction")
+
+print("3. Multiplication")
+
+print("4. and Division")
+
+input("\nPress Enter to continue.")
+
+a = input("\nPlease enter your first number (e.g., an integer or decimal, like 10 or 5.5):")
+
+b = input("\nPlease enter your second number (e.g., an integer or decimal, like 10 or 5.5):")
