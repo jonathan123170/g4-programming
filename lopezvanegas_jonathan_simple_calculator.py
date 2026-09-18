e751b6cc81ace1a4a4c5aab32030c033b2182113
jonathan123170 <jonathan123170@mcpsva.org>
2026-09-18 15:13:55 -0400

    lol

--- lopezvanegas_jonathan_simple_calculator.py
+++ lopezvanegas_jonathan_simple_calculator.py
@@ -1,6 +1,6 @@
 print("Hello! Welcome to the Simple Calculator!")
 
-print("\nThis simple calculator is a calculator that will help with 2 numbers on the 4 basic symbols:")
+print("\nThis simple calculator is a calculator that'll help with 2 numbers on the 4 basic symbols:")
 
 print("1. Addition")
 
@@ -8,11 +8,24 @@ print("2. Subtraction")
 
 print("3. Multiplication")
 
-print("4. and Division")
+print("& 4. Division")
 
 input("\nPress Enter to continue.")
 
-a = input("\nPlease enter your first number (e.g., an integer or decimal, like 10 or 5.5):")
+a = int(input("\nPlease enter your first number (e.g., an integer or decimal, like 10 or 5.5):"))
 
-b = input("\nPlease enter your second number (e.g., an integer or decimal, like 10 or 5.5):")
+b = int(input("\nPlease enter your second number (e.g., an integer or decimal, like 10 or 5.5):"))
 
+print("\nHere are your answers:")
+
+print(f"Sum: {a+b}")
+
+print(f"Difference: {a-b}")
+
+print(f"Product: {a*b}")
+
+print(f"Quotient: {a/b}")
+
+print("\nThank you for using the simple calculator!")
+
+print("\nHave a good day!")
