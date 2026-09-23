6cab0221607be1b8ae44eec908c9d82a4d6b7105
jonathan123170 <jonathan123170@mcpsva.org>
2026-09-23 15:15:16 -0400

    lol

---
+++ lopezvanegas_jonathan_conditional_calculator.py
@@ -0,0 +1,27 @@
+print("Hello! Welcome to the Simple Calculator!")
+
+print("\nThis conditional calculator is a calculator that'll help with 2 numbers on the 4 basic symbols:")
+
+print("1. Addition")
+
+print("2. Subtraction")
+
+print("3. Multiplication")
+
+print("& 4. Division")
+
+input("\nPress Enter to continue.")
+
+a = int(input("\nPlease enter your first number (e.g., an integer or decimal, like 10 or 5.5):"))
+
+b = input("\nPlease type in the operation you would like to do (like +,-,*,/)")
+
+c = int(input("\nPlease enter your second number (e.g., an integer or decimal, like 10 or 5.5):"))
+
+print("\nHere is your answer:")
+
+print(f"{a+b+c}")
+
+print("\nThank you for using the simple calculator!")
+
+print("\nHave a good day!")
