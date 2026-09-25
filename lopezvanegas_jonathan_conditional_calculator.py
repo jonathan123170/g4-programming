f75db6ec01108469ab719309ca50f9a82954ca23
jonathan123170 <jonathan123170@mcpsva.org>
2026-09-25 14:19:57 -0400

    lol

--- lopezvanegas_jonathan_conditional_calculator.py
+++ lopezvanegas_jonathan_conditional_calculator.py
@@ -1,6 +1,6 @@
-print("Hello! Welcome to the Simple Calculator!")
+print("Hello! Welcome to the Conditional Calculator!")
 
-print("\nThis conditional calculator is a calculator that'll help with 2 numbers on the 4 basic symbols:")
+print("\nThis conditional calculator is a calculator that'll help with 2 numbers on the 4 basic operations:")
 
 print("1. Addition")
 
@@ -14,14 +14,22 @@ input("\nPress Enter to continue.")
 
 a = int(input("\nPlease enter your first number (e.g., an integer or decimal, like 10 or 5.5):"))
 
-b = input("\nPlease type in the operation you would like to do (like +,-,*,/)")
+b = input("\nPlease type in the operation you would like to do (like +,-,*,/):")
 
 c = int(input("\nPlease enter your second number (e.g., an integer or decimal, like 10 or 5.5):"))
 
 print("\nHere is your answer:")
 
-print(f"{a+b+c}")
+if b == "+":
+    print(f"{a} + {c} = {a+c}")
+elif b == "-":
+    print(f"{a} - {c} = {a-c}")
+elif b == "*":
+    print(f"{a} * {c} = {a*c}")
+elif b == "/":
+    print(f"{a} / {c} = {a/c}")
 
-print("\nThank you for using the simple calculator!")
+
+print("\nThank you for using the conditional calculator!")
 
 print("\nHave a good day!")
